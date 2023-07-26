4249b2facdb03f0509da8b2faaae3689a53dfff1
vivek <vivek.mannava@ledlenser.com>
2023-07-26 12:48:49 +0200

    Added basic form for order scenario

--- mdr_rasa/models/cocktail_party_Regex/actions/actions.py
+++ mdr_rasa/models/cocktail_party_Regex/actions/actions.py
@@ -11,21 +11,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
+from rasa_sdk.events import SlotSet
 
 class ActionDealWithSystemMessage(Action):
 
@@ -39,3 +25,16 @@ class ActionDealWithSystemMessage(Action):
         dispatcher.utter_message(text="Hello World!")
 
         return []
+
+class ActionClearSlot(Action):
+    def name(self) -> Text:
+        return "action_clear_slots"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+        # add all the slots you wish to clear, here:
+        return [SlotSet("order", None), SlotSet("places",None)]
