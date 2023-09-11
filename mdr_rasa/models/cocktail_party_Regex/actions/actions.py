de73424989ee6a9a70db4848c657636e801d89ed
Nada Aweaa <44225138+NadaAweaa@users.noreply.github.com>
2023-09-11 19:06:34 +0000

    added validation in actions

--- mdr_rasa/models/cocktail_party_Regex/actions/actions.py
+++ mdr_rasa/models/cocktail_party_Regex/actions/actions.py
@@ -11,6 +11,40 @@ from rasa_sdk.types import DomainDict
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 
+
+class ValidateSimpleOrderForm(FormValidationAction):
+    
+    def name(self) -> Text:
+        return "validate_simple_order_form"
+
+    def __init__(self) -> None:
+        self.valid_locations = ["living room", "kitchen", "dining room","hall"]
+        self.valid_objects = ["pringles","windex bottle","soup can","shirt","spatula"]
+
+    def validate_location(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        if value.lower() not in self.valid_locations:
+            dispatcher.utter_message("Sorry, I don't recognize that location.")
+            return {"location": None}
+        return {"location": value}
+
+    def validate_object(
+        self,
+        value: Text,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        if value.lower() not in self.valid_objects:
+            dispatcher.utter_message("Sorry, I don't recognize that object.")
+            return {"object": None}
+        return {"object": value}
+
 class ActionCustomFallback(Action):
     def name(self) -> Text:
         return "action_fallback_response"
