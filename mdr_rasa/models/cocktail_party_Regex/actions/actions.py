b4ae03666507308fa3a9661841ea3e8e1df86c6f
NadaAweaa <nadaa.elmouniem@gmail.com>
2023-09-21 09:04:15 +0100

    Some minor changes to get over circuitbreaker

--- mdr_rasa/models/cocktail_party_Regex/actions/actions.py
+++ mdr_rasa/models/cocktail_party_Regex/actions/actions.py
@@ -9,7 +9,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.types import DomainDict
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, Restarted
 
 
 class ValidateSimpleOrderForm(FormValidationAction):
@@ -19,7 +19,7 @@ class ValidateSimpleOrderForm(FormValidationAction):
 
     def __init__(self) -> None:
         self.valid_locations = ["living_shelf", "dining_room","reading_room"]
-        self.valid_objects = ["pringles","windex_bottle","soup","shirt","spatula"]
+        self.valid_objects = ["pringles","windex_bottle","soup","tshirt","spatula"]
 
     def validate_places(
         self,
@@ -30,7 +30,8 @@ class ValidateSimpleOrderForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         if value.lower() not in self.valid_locations:
             # dispatcher.utter_message("Sorry, I don't recognize that location.")
-            dispatcher.utter_template("utter_ask_places_again", tracker)
+
+            dispatcher.utter_message(template="utter_ask_places_again")
             return {"places": None}
         return {"places": value}
 
@@ -43,7 +44,7 @@ class ValidateSimpleOrderForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         if value.lower() not in self.valid_objects:
             # dispatcher.utter_message("Sorry, I don't recognize that object.")
-            dispatcher.utter_template("utter_ask_order_again", tracker)
+            dispatcher.utter_message(template="utter_ask_order_again")
             return {"order": None}
         return {"order": value}
 
@@ -57,10 +58,10 @@ class ActionCustomFallback(Action):
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         # Send a message to the user
-        dispatcher.utter_template("utter_fallback")
+        dispatcher.utter_message(template="utter_fallback")
 
         # Return an empty list
-        return []
+        return[]
 
 class ActionClearSlot(Action):
     def name(self) -> Text:
@@ -75,3 +76,4 @@ class ActionClearSlot(Action):
         # add all the slots you wish to clear, here:
         # TODO: can also use return [AllSlotsReset()] if always clearing all slots. 
         return [SlotSet("order", None), SlotSet("places",None), SlotSet("confirmation", None)]
+    
